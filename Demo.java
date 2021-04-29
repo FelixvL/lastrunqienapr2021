31596419ebffec2a6e7fd5ffdd0230052f7ed690
felixvl <digitaleservice@gmail.com>
2021-04-29 11:36:34 +0200

    AS

--- Demo.java
+++ Demo.java
@@ -1,16 +1,16 @@
-class Demo{  //    AR
+class Demo{  //    AS
 	public static void main(String[] args) {
 		lopen();
 	}
 	static void lopen(){
 		System.out.println("in lopen");
-		new Fiets().fietsen(4);
+		new Fiets().fietsen(6);
 	}
 }
 
 class Fiets{
 	void fietsen(int getal){
-		System.out.println("fietsen in fiets"+getal+getal);
+		System.out.println(getal+getal+"fietsen");
 	}
 }
 
