ca98be2785743782337baa8fe7a1a6c2dd844f58
felixvl <digitaleservice@gmail.com>
2021-04-29 11:23:09 +0200

    AQ

--- Demo.java
+++ Demo.java
@@ -1,6 +1,10 @@
-class Demo{  // AW test
+class Demo{  //    AQ
 	public static void main(String[] args) {
 		System.out.println(args.length);
+		lopen();
+	}
+	void lopen(){
+		System.out.println("in lopen");
 	}
 }
 
