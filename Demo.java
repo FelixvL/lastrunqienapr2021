52393aad71f66d4f26f57b38a636bab25894ea75
felixvl <digitaleservice@gmail.com>
2021-04-30 09:28:36 +0200

    BI

--- Demo.java
+++ Demo.java
@@ -1,4 +1,4 @@
-class Demo{  //    BF
+class Demo{  //    BI
 	public static void main(String[] args) throws Exception{
 		System.out.println("Start");
 		Cola cola = new Cola();
@@ -15,6 +15,11 @@ class Demo{  //    BF
 class Cola{
 	void drinken() throws Exception{
 		throw new RuntimeException();
+	}
+}
+class ColaZero extends Cola{
+	void drinken(){
+		System.out.println("0 calorien");
 	}
 }
 
