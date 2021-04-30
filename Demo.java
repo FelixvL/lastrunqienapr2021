a1691ddff914564cf742ad05ec35c3294a3eff77
felixvl <digitaleservice@gmail.com>
2021-04-30 09:11:03 +0200

    BZ

--- Demo.java
+++ Demo.java
@@ -1,12 +1,14 @@
-class Demo{  //    BX
+class Demo{  //    BZ
 	public static void main(String[] args) {
+		System.out.println("Start");
+		Cola cola = new Cola();
+		System.out.println("Einde");
 	}
 }
 class Cola{
 	void drinken(){
-		throw new Error();
+		throw new Exception();
 	}
-
 }
 
 
