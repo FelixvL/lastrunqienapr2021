cfa7c59e039917a90ea72a5f7b56f0cc14f7b9e0
felixvl <digitaleservice@gmail.com>
2021-04-30 09:44:08 +0200

    BM

--- Demo.java
+++ Demo.java
@@ -1,22 +1,28 @@
-class Demo{  //    BL
-	public static void main(String[] args){
+class Demo{  //    BM
+	public static void main(String[] args)throws Exception{
 		System.out.println("Start");
 		Cola cola = new ColaZero();
 		try{
+			int a = 5;
 			cola.drinken();
 		}finally{
+			a = 6;
 			System.out.println("bijna weekend");
 		}
-		System.out.println("Einde");
+		System.out.println("Einde" + a);
 	}
 }
 class Cola{
 	void drinken() throws Exception{
-		throw new RuntimeException();
+		try{
+			throw new Exception();
+		}catch(Error e){
+			System.out.println("ok");
+		}
 	}
 }
 class ColaZero extends Cola{
-	void drinken(){
+	void drinken(int a){
 		System.out.println("0 calorien");
 	}
 }
