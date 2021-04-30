352e0fd96e49b426249c3f9b6ad175f742f1659d
felixvl <digitaleservice@gmail.com>
2021-04-30 09:20:15 +0200

    BD

--- Demo.java
+++ Demo.java
@@ -1,8 +1,14 @@
-class Demo{  //    BC
+class Demo{  //    BD
 	public static void main(String[] args) throws Exception{
 		System.out.println("Start");
 		Cola cola = new Cola();
-		cola.drinken();
+		try{
+			cola.drinken();
+		}catch(Exception e){
+			System.out.println("hij is gevangen");
+		}catch(RuntimeException re){
+			System.out.println("nogmaals");
+		}
 		System.out.println("Einde");
 	}
 }
