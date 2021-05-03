8b320a0ac6f5708a1bee557247744538ffbe347c
felixvl <digitaleservice@gmail.com>
2021-05-03 12:25:07 +0200

    GT

--- Demo.java
+++ Demo.java
@@ -1,8 +1,8 @@
-class Demo{  // GS              
+class Demo{  // GT              
 	public static void main(String[] args){
 		int[][] oeps[] = {{{},{4,3,2},{5}},{},null,{null, {4,5,6}}};
 		int[] ff = oeps[3][1];
-		System.out.println(ff[0].length);
+		System.out.println(ff.length);
 		System.out.println("gelukt");
 	}	
 }
