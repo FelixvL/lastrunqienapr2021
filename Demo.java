d39e5479188a54ccde423ae6137190fcb8332946
felixvl <digitaleservice@gmail.com>
2021-05-03 12:19:20 +0200

    GM

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
-class Demo{  // GL                
+class Demo{  // GM               
 	public static void main(String[] args){
 		int[][] oeps[] = {{{}},{},null,{null, {4,5,6}}};
-		System.out.println(oeps.length);
+		System.out.println(oeps[0].length);
 		System.out.println("gelukt");
 	}	
 }
