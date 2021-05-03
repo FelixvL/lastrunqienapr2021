2892da6131b94023ccdbd31a8ab2e1451a174d36
felixvl <digitaleservice@gmail.com>
2021-05-03 12:16:06 +0200

    GJ

--- Demo.java
+++ Demo.java
@@ -1,37 +1,6 @@
-class Demo{  // GI                
+class Demo{  // GJ                
 	public static void main(String[] args){
-		System.out.println("start");
-		Groen g = new Groen();
-		switch(g.rood.a){
-			default:
-				System.out.println("in default");
-			case 34:
-				System.out.println("in 34");
-		}
-		System.out.println("kleur");
-		g.rood.a();
+		int[] oeps[] = {{},{},null,{4,6,2}};
+		System.out.println("gelukt");
 	}	
-}
-class Groen{
-	final static LightRood rood = null;
-	Groen(){
-		System.out.println("Green");
-	}
-}
-class Rood{
-	static long a = 34;
-	Rood(){
-		System.out.println("Blauw");
-	}
-	int a(){
-		System.out.println("lightrood");
-		return 66;
-	}
-}
-class LightRood extends Rood{
-	static long a = 35;
-	int a(){
-		System.out.println("rood");
-		return 88;
-	}
 }
