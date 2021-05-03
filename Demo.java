8cf9a555ae1e5cf257b1f9ca5e7f178a5b16a6d3
felixvl <digitaleservice@gmail.com>
2021-05-03 09:18:56 +0200

    DV

--- Demo.java
+++ Demo.java
@@ -1,19 +1,17 @@
-class Demo{  // DU
+class Demo{  // DV
 	public static void main(String[] args){
 		System.out.println("Start");
 		Start s = new Start();
-		s.daarGaanWe();
-		System.out.println("Einde");
+		int getal = s.daarGaanWe();
+		System.out.println("Einde" + getal);
 	}
 }
 class Start{
 	int daarGaanWe(){
-		if(false)
-			System.out.println("Een");
-			System.out.println("Twee");
+		if(true)
+			return 4;
 		else
 			System.out.println("Drie");
-			return 4;
 	}
 }
 
