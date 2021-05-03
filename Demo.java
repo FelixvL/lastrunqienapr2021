32f6543fd15f021391115bd11c221666e57ed97c
felixvl <digitaleservice@gmail.com>
2021-05-03 12:16:46 +0200

    GK

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
-class Demo{  // GJ                
+class Demo{  // GK                
 	public static void main(String[] args){
-		int[] oeps[] = {{},{},null,{4,6,2}};
+		int[][] oeps[] = {{{}},{},null,{4,6,2}};
 		System.out.println("gelukt");
 	}	
 }
