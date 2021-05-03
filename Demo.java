c6c285c705ba75899b7acf17aea261dede6d0a9d
felixvl <digitaleservice@gmail.com>
2021-05-03 12:29:23 +0200

    HB

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
-class Demo{  // HA             
+class Demo{  // HB             
 	public static void main(String[] args){
-		for(int a = 3, long b = 6 ;  ;  a++){
+		for(int a = 3, b = 6 ; false ;  a++){
 			System.out.println("1 x");
 		}
 	}	
