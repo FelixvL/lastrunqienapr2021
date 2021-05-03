16e8aa5a270215061e80b469c67d4c16ac5d3bd8
felixvl <digitaleservice@gmail.com>
2021-05-03 11:51:03 +0200

    GZ

--- Demo.java
+++ Demo.java
@@ -1,9 +1,10 @@
-class Demo{  // GY                
+class Demo{  // GZ                
 	public static void main(String[] args){
 		Groen g = new Groen();
 		g.rood = new LightRood();
 		System.out.println(g.rood.a);
 		System.out.println("kleur");
+		g.rood.a();
 	}	
 }
 
@@ -13,8 +14,18 @@ class Groen{
 
 class Rood{
 	int a = 34;
+	static int a(){
+		System.out.println("lightrood");
+		a = 55;
+		return 66;
+	}
 }
 
 class LightRood extends Rood{
 	int a = 35;
+	static int a(){
+		System.out.println("lightrood");
+		a = 77;
+		return 88;
+	}
 }
